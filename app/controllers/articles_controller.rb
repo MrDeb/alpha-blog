7baf4a0c2d31b93b285522ab6ad79ba85af22249
MrDeb <alexzibaicai@hotmail.com>
2017-01-10 01:39:28 -0500

    production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,14 +5,12 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.save
-    redirect_to article_show(@article)
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+   def article_params
+    params.require(:article).permit(:title, :description)
+   end
 end
