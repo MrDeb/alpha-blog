d29ecb1a9d12daa613ac8d1b2f90856c4440ca13
Alex Wang <mrdeb@Alexs-MacBook-Pro.local>
2017-03-13 23:42:25 -0400

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+
   def set_user
     @user = User.find(params[:id])
   end
